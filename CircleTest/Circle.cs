0ed8e66c4509b6a4a8377018d522e8ad60cb2eed
meetshingala <85341895+meetshingala@users.noreply.github.com>
2023-02-09 01:04:52 -0500

    circel test

---
+++ CircleTest/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CircleTest
+{
+    internal class Circle
+    {
+        private int v;
+
+        public Circle()
+        {
+        }
+
+        public Circle(int v)
+        {
+            this.v = v;
+        }
+
+        internal double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double GetCircumference()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int GetRadius()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetRadius(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CircleTest/Class1.cs
+++ CircleTest/Class1.cs
@@ -1,12 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace CircleTest
 {
     public class Class1
     {
+        Circle circle = new Circle();
+        [NUnit.Framework.SetUp]
+        public void Setup()
+        {
+            Circle circle = new Circle(5);
+        }
+
+        [Test]
+        public void GetRadius_SetRadius0_Return0()
+        {
+            circle.SetRadius(0);
+            int expectedResult = 0;
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetRadius_SetRadiusMinus1_ReturnsMinus1()
+        {
+            circle.SetRadius(-1);
+            int expectedResult = -1;
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetRadius_SetRadius10_Returns10()
+        {
+            circle.SetRadius(10);
+            int expectedResult = 10;
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void SetRadius_SetRadius28_Returns28()
+        {
+            int expectedResult = 28;
+            circle.SetRadius(28);
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void SetRadius_SetRadius0_Returns0()
+        {
+            int expectedResult = 0;
+            circle.SetRadius(0);
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void SetRadius_SetRadiusMinus12_ReturnsMinus12()
+        {
+            int expectedResult = -12;
+            circle.SetRadius(-12);
+            int actualResult = circle.GetRadius();
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void GetCircumference_SetRadiusMinus6_Returns37Point69()
+        {
+            circle.SetRadius(-6);
+            double expectedResult = -37.69;
+            double actualResult = circle.GetCircumference();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
+        [Test]
+        public void GetCircumference_SetRadius7_Returns43Point98()
+        {
+            circle.SetRadius(7);
+            double expectedResult = 43.98;
+            double actualResult = circle.GetCircumference();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
+        [Test]
+        public void GetCircumference_SetRadius0_Returns0()
+        {
+            circle.SetRadius(0);
+            double expectedResult = 0;
+            double actualResult = circle.GetCircumference();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
+        [Test]
+        public void GetArea_SetRadius0_Returns0()
+        {
+            circle.SetRadius(0);
+            double expectedResult = 0;
+            double actualResult = circle.GetArea();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
+        [Test]
+        public void GetArea_SetRadius12_Returns452Point39()
+        {
+            circle.SetRadius(12);
+            double expectedResult = 452.39;
+            double actualResult = circle.GetArea();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
+        [Test]
+        public void GetArea_SetRadiusMinus11_Returns380Point13()
+        {
+            circle.SetRadius(-11);
+            double expectedResult = 380.13;
+            double actualResult = circle.GetArea();
+            Assert.AreEqual(expectedResult, actualResult, 0.01);
+        }
     }
 }
