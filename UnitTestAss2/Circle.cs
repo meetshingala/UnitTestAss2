445d7bc646be25c60d0d061bcc9741a826e102c8
meetshingala <85341895+meetshingala@users.noreply.github.com>
2023-02-09 00:48:51 -0500

    console app complete

--- UnitTestAss2/Circle.cs
+++ UnitTestAss2/Circle.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace UnitTestAss2
 {
-    internal class Circle
+    public class Circle
     {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetCircumference()
+        {
+
+            return 2 * Math.PI * radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
     }
 }
--- UnitTestAss2/Program.cs
+++ UnitTestAss2/Program.cs
@@ -10,6 +10,55 @@ namespace UnitTestAss2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Welcome to Circle Program");
+                Console.WriteLine("Please Enter the radius of the circle: ");
+                int radius = Convert.ToInt32(Console.ReadLine());
+
+                Circle circle = new Circle(radius);
+
+                int choice;
+                do
+                {
+                    Console.WriteLine("1. Get Circle Radius");
+                    Console.WriteLine("2. Change Circle Radius");
+                    Console.WriteLine("3. Get Circle Circumference");
+                    Console.WriteLine("4. Get Circle Area");
+                    Console.WriteLine("5. Exit");
+
+                    Console.WriteLine("Enter your choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Radius of the circle is: " + circle.GetRadius());
+                            break;
+                        case 2:
+                            Console.WriteLine("Please Enter the new radius: ");
+                            int newRadius = Convert.ToInt32(Console.ReadLine());
+                            circle.SetRadius(newRadius);
+                            break;
+                        case 3:
+                            Console.WriteLine("Circumference of the circle is: " + circle.GetCircumference());
+                            break;
+                        case 4:
+                            Console.WriteLine("Area of the circle is: " + circle.GetArea());
+                            break;
+                        case 5:
+                            Console.WriteLine("Good Bye. See you super soon.");
+                            break;
+                        default:
+                            Console.WriteLine("Please Enter from the options mentioned above.");
+                            break;
+                    }
+                } while (choice != 5);
+            }
+            catch
+            {
+                Console.WriteLine("Please Enter Valid Values.");
+            }
         }
     }
 }
